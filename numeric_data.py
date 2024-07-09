7fcdaad3deb9f82a6329f9c4e9915edb4bf44d75
Marie Mule <99121400+mariemulemuthoni@users.noreply.github.com>
2024-07-09 11:50:54 +0300

    Creating a Git Repository

---
+++ numeric_data.py
@@ -0,0 +1,45 @@
+print("Python has three numeric types: int, float, and complex")
+
+#Creating a variable & Introducing the int data type
+myValue = 1
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Introducing the float data type
+myValue2 = 3.14
+
+print(myValue2)
+
+print(type(myValue2))
+
+print(str(myValue2) + " is of the data type " + str(type(myValue2)))
+
+#Introducing the complex data type
+myValue3 = 5j
+
+print(myValue3)
+
+print(type(myValue3))
+
+print(str(myValue3) + " is of the data type " + str(type(myValue3)))
+
+#Introducing the bool data type
+myValue4 = True
+
+print(myValue4)
+
+print(type(myValue4))
+
+print(str(myValue4) + " is of the data type " + str(type(myValue4)))
+
+myValue5 = False
+
+print(myValue5)
+
+print(type(myValue5))
+
+print(str(myValue5) + " is of the data type " + str(type(myValue5)))
